32ffc3a45e20d04f4b94f462546413ce98cbb9d2
Tony Cebzanov <tonycpsu@gmail.com>
2024-01-07 10:11:01 -0500

    Fix stripHtml call

--- server.ts
+++ server.ts
@@ -226,7 +226,7 @@ function genSnippet(feed) {
   const post = getPost(feed.post, false);
   const reply = (AppBskyFeedDefs.isPostView(feed.reply?.parent))
   return (
-    stripHtml(post.text) ?? (reply && stripHtml(reply.text)) ?? `Post by ${handle}`
+    stripHtml(post.text).result ?? (reply && stripHtml(reply.text).result) ?? `Post by ${handle}`
   ).substring(0, MAX_SNIPPET_LENGTH);
 }
 
